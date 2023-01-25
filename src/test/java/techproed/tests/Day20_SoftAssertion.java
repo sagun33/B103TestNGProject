4a404b278cb9fb69514aabbe2ce1476f388376fd
sagun33 <salihguenay33@gmail.com>
2023-01-25 20:58:04 +0100

    B102TestNGProject

---
+++ src/test/java/techproed/tests/Day20_SoftAssertion.java
@@ -0,0 +1,20 @@
+package techproed.tests;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class Day20_SoftAssertion {
+    @Test
+    public void testsoftAssertTest() {
+        //1.Soft Assert objesi olustur
+        SoftAssert softAssert=new SoftAssert();
+        System.out.println("Satir 14");
+        softAssert.assertEquals(2,5);
+        System.out.println("SAtir 16");
+        softAssert.assertTrue("Java".contains("U"));
+        System.out.println("Satir 18");
+        softAssert.assertTrue(true);
+        System.out.println("Satir 28");
+        softAssert.assertAll();
+    }
+}
